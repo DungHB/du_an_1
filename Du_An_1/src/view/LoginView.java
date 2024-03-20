5751b0598947feb134f5d6602be7a25d6936d0e4
ductoasang <duchaph46353@fpt.edu.vn>
2024-03-20 18:58:17 +0700

    ducha

--- Du_An_1/src/view/LoginView.java
+++ Du_An_1/src/view/LoginView.java
@@ -32,11 +32,11 @@ public class LoginView extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 470, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 385, Short.MAX_VALUE)
         );
 
         pack();
